9ca759e102d773d54bd135082191c2d43b3fd507
Lenka Havlíčková <100620477+LenkaHavlickova@users.noreply.github.com>
2023-10-31 21:13:22 +0100

    Created new project FoxShopNullPointerNinjasOtocyon, set applicaction.properties, set en.variables

---
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/FoxShopNullPointerNinjasOtocyonApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.foxshopnullpointerninjasotocyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxShopNullPointerNinjasOtocyonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxShopNullPointerNinjasOtocyonApplication.class, args);
+	}
+
+}
