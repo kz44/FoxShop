2eefac124196a80624fee9d47c27db252df513bd
Anna Karner <zanna.karner@gmail.com>
2023-11-02 19:28:39 +0100

    Changed @Controller to @Restcontroller

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/UserController.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/UserController.java
@@ -3,7 +3,7 @@ package com.greenfoxacademy.foxshopnullpointerninjasotocyon.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
