4391ec5501fa14b4745fdc1952c87fa5ccbfc756
Anna Karner <zanna.karner@gmail.com>
2023-11-02 14:28:56 +0100

    Rate model created

---
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/models/Rate.java
@@ -0,0 +1,29 @@
+package com.greenfoxacademy.foxshopnullpointerninjasotocyon.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Rate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne
+    private User sender;
+
+    @ManyToOne
+    private User receiver;
+}
