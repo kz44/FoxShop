8aac5a15782ccbe5759357f55bf4b48770e9bea4
Anna Karner <zanna.karner@gmail.com>
2023-11-04 11:46:56 +0100

    I set nullable= false to every field, and set @JoinColumn/@JoinTable to the relationships

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/models/Advertisement.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/models/Advertisement.java
@@ -24,21 +24,36 @@ public class Advertisement {
     @Column(nullable = false)
     private Integer price;
 
+    @Column(nullable = false)
     @ManyToOne
+    @JoinColumn(name = "location_id")
     private Location location;
 
+    @Column(nullable = false)
     @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
+    @Column(nullable = false)
     @ManyToOne
+    @JoinColumn(name = "delivery_method_id")
     private DeliveryMethod deliveryMethod;
 
+    @Column(nullable = false)
     @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
 
+    @Column(nullable = false)
     @ManyToOne
+    @JoinColumn(name = "condition_id")
     private Condition condition;
 
+    @Column(nullable = false)
     @ManyToMany
+    @JoinTable(
+            name = "advertisement_image",
+            joinColumns = @JoinColumn(name = "advertisement_id"),
+            inverseJoinColumns = @JoinColumn(name = "image_paths_id"))
     private Set<ImagePaths> imagePaths;
 }
