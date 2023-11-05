df00132ae539acfb5a31afa992d2efeab83bdbef
seprenyibalazs <80784538+seprenyibalazs@users.noreply.github.com>
2023-11-05 14:22:35 +0100

    DeliveryMethod model is created

---
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/models/DeliveryMethod.java
@@ -0,0 +1,25 @@
+package com.greenfoxacademy.foxshopnullpointerninjasotocyon.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryMethod {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "location")
+    private Set<Advertisement> advertisements;
+}
