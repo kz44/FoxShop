ce89b7e839afc81f57d53572930de7733bcdcedb
Anna Karner <zanna.karner@gmail.com>
2023-11-06 11:19:01 +0100

    added unique = true to email field

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/models/User.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/models/User.java
@@ -33,7 +33,7 @@ public class User {
     @Column(nullable = false)
     private LocalDate dateOfBirth;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
