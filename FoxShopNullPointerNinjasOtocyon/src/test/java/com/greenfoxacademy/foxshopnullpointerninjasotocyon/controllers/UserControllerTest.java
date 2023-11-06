37787550b6e2388f030b6c19900f64d515cf7f2f
Lenka Havlíčková <100620477+LenkaHavlickova@users.noreply.github.com>
2023-11-06 16:25:12 +0100

    Added ErrorMessageDTO to dtos, added method  to UserController which should be called from registration endpoint. Added unit tests for this method.

---
+++ FoxShopNullPointerNinjasOtocyon/src/test/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/UserControllerTest.java
@@ -0,0 +1,55 @@
+package com.greenfoxacademy.foxshopnullpointerninjasotocyon.controllers;
+
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.ErrorMessageDTO;
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.RegisterDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    private final UserController userController = new UserController();
+
+
+    @Test
+    public void registrationNullCheckResponseIsOK() {
+        RegisterDto registerDto = new RegisterDto("JohnSmith", "John", "Smith", "john.smith@test.com", "password", LocalDate.of(1980, 11, 1));
+        ResponseEntity<?> response = userController.registrationNullCheck(registerDto);
+        assertEquals(ResponseEntity.ok().build(), response);
+    }
+
+    @Test
+    public void registrationNullCheckResponseIsBadMissingUsername() {
+        RegisterDto registerDto = new RegisterDto(null, "John", "Smith", "john.smith@test.com", "password", LocalDate.of(1980, 11, 1));
+        ResponseEntity<?> response = userController.registrationNullCheck(registerDto);
+        assertInstanceOf(ErrorMessageDTO.class, response.getBody());
+        ErrorMessageDTO errorMessageDTO = (ErrorMessageDTO) response.getBody();
+        assertNotNull(errorMessageDTO);
+        assertEquals("There are missing some data in your request: username.", errorMessageDTO.getError());
+    }
+
+    @Test
+    public void registrationNullCheckResponseIsBadMissingPasswordAndDateOfBirth() {
+        RegisterDto registerDto = new RegisterDto("johnSmith", "John", "Smith", "john.smith@test.com", null, null);
+        ResponseEntity<?> response = userController.registrationNullCheck(registerDto);
+        assertInstanceOf(ErrorMessageDTO.class, response.getBody());
+        ErrorMessageDTO errorMessageDTO = (ErrorMessageDTO) response.getBody();
+        assertNotNull(errorMessageDTO);
+        assertEquals("There are missing some data in your request: password, date of birth.", errorMessageDTO.getError());
+    }
+
+    @Test
+    public void registrationNullCheckResponseIsBadMissingAllData() {
+        RegisterDto registerDto = new RegisterDto(null, null, null, null, null, null);
+        ResponseEntity<?> response = userController.registrationNullCheck(registerDto);
+        assertInstanceOf(ErrorMessageDTO.class, response.getBody());
+        ErrorMessageDTO errorMessageDTO = (ErrorMessageDTO) response.getBody();
+        assertNotNull(errorMessageDTO);
+        assertEquals("There are missing some data in your request: username, first name, last name, email, password, date of birth.", errorMessageDTO.getError());
+    }
+
+
+}
