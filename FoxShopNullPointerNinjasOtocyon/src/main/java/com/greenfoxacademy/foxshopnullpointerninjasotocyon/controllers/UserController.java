e490aaf89fa3b2eb25c0e2b4d621453a11b78f02
Lenka Havlíčková <100620477+LenkaHavlickova@users.noreply.github.com>
2023-11-07 18:48:22 +0100

    Added LoginDTO, TokenResponseDTO, null check for login endpoint, login endpoint without authentication and without token generation

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/UserController.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/UserController.java
@@ -1,9 +1,47 @@
 package com.greenfoxacademy.foxshopnullpointerninjasotocyon.controllers;
+
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.ErrorMessageDTO;
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.LoginDTO;
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.TokenResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody(required = false) LoginDTO loginDTO) {
+        if (loginDTO == null) {
+            return ResponseEntity.badRequest().body(new ErrorMessageDTO("There is missing the body of request with all data for login."));
+        }
+        ResponseEntity<?> response = nullCheckLogin(loginDTO);
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            return response;
+        }
+        //TODO need to do authentication and generate token
+        return ResponseEntity.ok().body(new TokenResponseDTO()); //TODO add token
+    }
+
+    public ResponseEntity<?> nullCheckLogin(LoginDTO loginDTO) {
+        List<String> errors = new ArrayList<>();
+        if (loginDTO.getEmail() == null && loginDTO.getUsername() == null) {
+            errors.add("Please, provide username or email for your authentication.");
+        }
+        if (loginDTO.getPassword() == null) {
+            errors.add("There is missing password in your login request.");
+        }
+        if (!errors.isEmpty()) {
+            String message = String.join(" ", errors);
+            return ResponseEntity.badRequest().body(new ErrorMessageDTO(message));
+        }
+        return ResponseEntity.ok().build();
+    }
 
 }
