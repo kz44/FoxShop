3f03a07db225bbfb5f72913f66c60bc4ecdc5f30
seprenyibalazs <80784538+seprenyibalazs@users.noreply.github.com>
2023-11-11 12:37:35 +0100

    delete unique

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/models/Category.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/models/Category.java
@@ -18,7 +18,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String name;
 
     @OneToMany(mappedBy = "parentCategory")
