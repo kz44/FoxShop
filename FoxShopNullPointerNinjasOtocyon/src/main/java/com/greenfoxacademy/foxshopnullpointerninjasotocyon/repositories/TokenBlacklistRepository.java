4df1ed3e9f50899011c78bcd0346f317ffd9bd8b
Lenka Iacovelli <lenkadan@gmail.com>
2023-11-12 14:24:43 +0100

    Updating the return type of the findByTokenEquals() method in the TokenBlacklistRepository to return BlacklistedJWTToken object type

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/repositories/TokenBlacklistRepository.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/repositories/TokenBlacklistRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.token.Token;
 
 public interface TokenBlacklistRepository extends JpaRepository<BlacklistedJWTToken, Long> {
-    Token findByTokenEquals(String token);
+    BlacklistedJWTToken findByTokenEquals(String token);
     boolean existsByToken (String token);
 }
