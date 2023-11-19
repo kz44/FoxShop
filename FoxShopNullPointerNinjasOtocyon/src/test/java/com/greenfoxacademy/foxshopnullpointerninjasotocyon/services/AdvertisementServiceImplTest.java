c406f010b838760dc0aef4c47e6df9042c424a99
Lenka Havlíčková <100620477+LenkaHavlickova@users.noreply.github.com>
2023-11-19 22:54:51 +0100

    Added tests for createNewAdvertisement with mocking security context.

--- FoxShopNullPointerNinjasOtocyon/src/test/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/services/AdvertisementServiceImplTest.java
+++ FoxShopNullPointerNinjasOtocyon/src/test/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/services/AdvertisementServiceImplTest.java
@@ -5,21 +5,23 @@ import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.AdvertisementRes
 import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.ErrorMessageDTO;
 import com.greenfoxacademy.foxshopnullpointerninjasotocyon.models.*;
 import com.greenfoxacademy.foxshopnullpointerninjasotocyon.repositories.*;
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.security.FoxUserDetails;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.test.context.support.WithMockUser;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 
 class AdvertisementServiceImplTest {
     @MockBean
@@ -76,28 +78,76 @@ class AdvertisementServiceImplTest {
     }
 
     @Test
-    @WithMockUser(username = "testUsername", authorities = {"USER"})
     void createNewAdvertisementEverythingOk() {
-        Authentication authentication = Mockito.mock(Authentication.class);
+        User user = new User(1L, "testUsername", "testFirstName", "testLastName",
+                LocalDate.of(1980, 1, 1),
+                "test@email.com", "password",
+                LocalDateTime.of(2022, 1, 1, 12, 13, 11),
+                null, null, null, null, null, null);
+        FoxUserDetails foxUserDetails = FoxUserDetails.fromUser(user);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(foxUserDetails, null, List.of(new SimpleGrantedAuthority("user")));
         SecurityContext securityContext = Mockito.mock(SecurityContext.class);
         Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
         SecurityContextHolder.setContext(securityContext);
-        Mockito.when(userRepository.findByUsername(any()))
-                .thenReturn(Optional.of(new User(1L, "testUsername", "testFirstName", "testLastName",
-                        LocalDate.of(1980, 1, 1),
-                        "test@email.com", "password",
-                        LocalDateTime.of(2022, 1, 1, 12, 13, 11),
-                        null, null, null, null, null, null)));
-
+        Mockito.when(userRepository.findByUsername("testUsername")).thenReturn(Optional.of(user));
         Mockito.when(categoryRepository.findById(1L)).thenReturn(Optional.of(new Category(1L, "testCategory", null, null, null)));
         Mockito.when(conditionRepository.findById(3L)).thenReturn(Optional.of(new Condition(1L, "testCondition", null)));
         Mockito.when(locationRepository.findById(4L)).thenReturn(Optional.of(new Location(1L, "testLocation", null)));
         Mockito.when(deliveryMethodRepository.findById(5L)).thenReturn(Optional.of(new DeliveryMethod(1L, "testDeliveryMethod", null)));
-
         AdvertisementDto advertisementDto = new AdvertisementDto("Title", "description", 100, 4L, 5L, 1L, 3L);
         ResponseEntity<?> response = advertisementService.createNewAdvertisement(advertisementDto);
         assertInstanceOf(AdvertisementResponseDto.class, response.getBody());
         AdvertisementResponseDto advertisementResponseDto = (AdvertisementResponseDto) response.getBody();
         assertNotNull(advertisementResponseDto);
+    }
+
+    @Test
+    void createNewAdvertisementWrongCategoryId() {
+        User user = new User(1L, "testUsername", "testFirstName", "testLastName",
+                LocalDate.of(1980, 1, 1),
+                "test@email.com", "password",
+                LocalDateTime.of(2022, 1, 1, 12, 13, 11),
+                null, null, null, null, null, null);
+        FoxUserDetails foxUserDetails = FoxUserDetails.fromUser(user);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(foxUserDetails, null, List.of(new SimpleGrantedAuthority("user")));
+        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        Mockito.when(userRepository.findByUsername("testUsername")).thenReturn(Optional.of(user));
+        Mockito.when(categoryRepository.findById(1L)).thenReturn(Optional.of(new Category(1L, "testCategory", null, null, null)));
+        Mockito.when(conditionRepository.findById(3L)).thenReturn(Optional.of(new Condition(1L, "testCondition", null)));
+        Mockito.when(locationRepository.findById(4L)).thenReturn(Optional.of(new Location(1L, "testLocation", null)));
+        Mockito.when(deliveryMethodRepository.findById(5L)).thenReturn(Optional.of(new DeliveryMethod(1L, "testDeliveryMethod", null)));
+        AdvertisementDto advertisementDto = new AdvertisementDto("Title", "description", 100, 4L, 5L, 2L, 3L);
+        ResponseEntity<?> response = advertisementService.createNewAdvertisement(advertisementDto);
+        assertInstanceOf(ErrorMessageDTO.class, response.getBody());
+        ErrorMessageDTO errorMessageDTO = (ErrorMessageDTO) response.getBody();
+        assertNotNull(errorMessageDTO);
+        assertEquals("There are some errors in your request: Wrong category id.", errorMessageDTO.getError());
+    }
+
+    @Test
+    void createNewAdvertisementWrongAllIds() {
+        User user = new User(1L, "testUsername", "testFirstName", "testLastName",
+                LocalDate.of(1980, 1, 1),
+                "test@email.com", "password",
+                LocalDateTime.of(2022, 1, 1, 12, 13, 11),
+                null, null, null, null, null, null);
+        FoxUserDetails foxUserDetails = FoxUserDetails.fromUser(user);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(foxUserDetails, null, List.of(new SimpleGrantedAuthority("user")));
+        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        Mockito.when(userRepository.findByUsername("testUsername")).thenReturn(Optional.of(user));
+        Mockito.when(categoryRepository.findById(1L)).thenReturn(Optional.of(new Category(1L, "testCategory", null, null, null)));
+        Mockito.when(conditionRepository.findById(3L)).thenReturn(Optional.of(new Condition(1L, "testCondition", null)));
+        Mockito.when(locationRepository.findById(4L)).thenReturn(Optional.of(new Location(1L, "testLocation", null)));
+        Mockito.when(deliveryMethodRepository.findById(5L)).thenReturn(Optional.of(new DeliveryMethod(1L, "testDeliveryMethod", null)));
+        AdvertisementDto advertisementDto = new AdvertisementDto("Title", "description", 100, 5L, 6L, 2L, 4L);
+        ResponseEntity<?> response = advertisementService.createNewAdvertisement(advertisementDto);
+        assertInstanceOf(ErrorMessageDTO.class, response.getBody());
+        ErrorMessageDTO errorMessageDTO = (ErrorMessageDTO) response.getBody();
+        assertNotNull(errorMessageDTO);
+        assertEquals("There are some errors in your request: Wrong category id. Wrong condition id. Wrong location id. Wrong delivery method id.", errorMessageDTO.getError());
     }
 }
