1ef936e20713c1b9a4dcd6bf0d1b866adcd16606
Anna Karner <zanna.karner@gmail.com>
2023-11-20 09:44:18 +0100

    created null check and exceptions

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/AuthController.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/AuthController.java
@@ -76,11 +76,18 @@ public class AuthController {
 
     @GetMapping("/user/roles")
     public ResponseEntity<?> getUserRoles() {
-        var user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("username", user.getUsername());
-        response.put("role", user.getRole());
-        return ResponseEntity.ok(response);
+        try {
+            var user = (FoxUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if (user != null) {
+                Map<String, Object> response = new HashMap<>();
+                response.put("username", user.getUsername());
+                response.put("role", user.getRoleName());
+                return ResponseEntity.ok(response);
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication principal is not an instance of FoxUserDetails");
+            }
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 }
