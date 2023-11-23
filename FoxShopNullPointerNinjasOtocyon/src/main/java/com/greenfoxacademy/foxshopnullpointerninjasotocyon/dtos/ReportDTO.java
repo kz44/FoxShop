d059773cf4cb658b368af866eebd254176db3528
Anna Karner <zanna.karner@gmail.com>
2023-11-23 22:41:04 +0100

    in ReportDTO no id needed

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/dtos/ReportDTO.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/dtos/ReportDTO.java
@@ -2,9 +2,6 @@ package com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos;
 
 import com.greenfoxacademy.foxshopnullpointerninjasotocyon.models.Advertisement;
 import com.greenfoxacademy.foxshopnullpointerninjasotocyon.models.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +13,6 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ReportDTO {
 
-    private Long id;
     private String description;
     private User sender;
     private Advertisement receiver;
