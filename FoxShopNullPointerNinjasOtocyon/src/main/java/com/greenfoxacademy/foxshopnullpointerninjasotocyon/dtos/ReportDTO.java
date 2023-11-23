99df430223cfbd91338027d3001c40b03b6ef071
Anna Karner <zanna.karner@gmail.com>
2023-11-23 22:00:22 +0100

    created ReportDTO

---
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/dtos/ReportDTO.java
@@ -0,0 +1,23 @@
+package com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos;
+
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.models.Advertisement;
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.models.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReportDTO {
+
+    private Long id;
+    private String description;
+    private User sender;
+    private Advertisement receiver;
+}
