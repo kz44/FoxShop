b29da59f5142d14453148caccda2b6da03f54835
Kollonay Zoltán <zoltan@kollonay.hu>
2023-11-23 19:40:49 +0100

    feat: added new closed field to Advertisement, added some custon query to AdvertisementRepository

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/repositories/AdvertisementRepository.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/repositories/AdvertisementRepository.java
@@ -1,7 +1,17 @@
 package com.greenfoxacademy.foxshopnullpointerninjasotocyon.repositories;
 
 import com.greenfoxacademy.foxshopnullpointerninjasotocyon.models.Advertisement;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdvertisementRepository extends JpaRepository<Advertisement, Long> {
+
+    Page<Advertisement> findByCategoryIdAndClosedFalse(Long categoryId, Pageable pageable);
+
+    Page<Advertisement> findByPriceLessThanEqualAndClosedFalse(Integer maxPrice, Pageable pageable);
+
+    Page<Advertisement> findByCategoryIdAndPriceLessThanEqualAndClosedFalse(Long categoryId, Integer maxPrice, Pageable pageable);
+
+    Page<Advertisement> findByClosedFalse(Pageable pageable);
 }
--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/services/AdvertisementService.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/services/AdvertisementService.java
@@ -1,10 +1,13 @@
 package com.greenfoxacademy.foxshopnullpointerninjasotocyon.services;
 
 import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.AdvertisementDto;
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.models.Advertisement;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 
 public interface AdvertisementService {
     ResponseEntity<?> nullCheckAdvertisement(AdvertisementDto advertisementDto);
 
     ResponseEntity<?> createNewAdvertisement(AdvertisementDto advertisementDto);
+    
 }
