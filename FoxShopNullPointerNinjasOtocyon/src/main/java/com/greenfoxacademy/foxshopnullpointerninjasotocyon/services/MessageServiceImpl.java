cf345d07cfeb633c84d41a1737e080ea10ac6b11
Anna Karner <zanna.karner@gmail.com>
2023-12-02 12:13:46 +0100

    added DTO-s instead of maps

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/services/MessageServiceImpl.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/services/MessageServiceImpl.java
@@ -1,5 +1,8 @@
 package com.greenfoxacademy.foxshopnullpointerninjasotocyon.services;
 
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.ConversationDTO;
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.ConversationInfoDTO;
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.SuccessMessageDTO;
 import com.greenfoxacademy.foxshopnullpointerninjasotocyon.models.Message;
 import com.greenfoxacademy.foxshopnullpointerninjasotocyon.models.User;
 import com.greenfoxacademy.foxshopnullpointerninjasotocyon.repositories.MessageRepository;
@@ -8,35 +11,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Service
 @AllArgsConstructor
 public class MessageServiceImpl implements MessageService {
 
     private MessageRepository messageRepository;
+    private UserService userService;
 
-    public ResponseEntity<?> getConversationInfo(User user) {
-        Map<String, Map<String, Object>> response = new HashMap<>();
-
+    public ResponseEntity<?> getConversationInfo() {
+        User user = userService.getUserFromSecurityContextHolder();
         Set<User> otherUsers = messageRepository.findOtherUsers(user);
 
+        if (otherUsers == null || otherUsers.isEmpty()) {
+            return ResponseEntity.ok(new SuccessMessageDTO("You have no conversations with other users yet."));
+        }
+        List<ConversationDTO> conversations = new ArrayList<>();
         for (User otherUser : otherUsers) {
-            Map<String, Object> conversationInfo = new HashMap<>();
-
             Message lastMessage = messageRepository.getLastMessage(user, otherUser);
             LocalDateTime lastMessageTime = lastMessage.getSent();
-            boolean lastMessageIsAlreadyRead = lastMessage.isAlreadyRead();
-            int numberOfMessages = messageRepository.countMessagesBetweenUsers(user, otherUser);
+            boolean isLastMessageAlreadyRead = lastMessage.isAlreadyRead();
+            long numberOfMessages = messageRepository.countMessagesBetweenUsers(user, otherUser);
 
-            conversationInfo.put("lastMessageTime", lastMessageTime);
-            conversationInfo.put("lastMessageIsAlreadyRead", lastMessageIsAlreadyRead);
-            conversationInfo.put("numberOfMessages", numberOfMessages);
-
-            response.put(otherUser.getUsername(), conversationInfo);
+            ConversationDTO conversationDTO = new ConversationDTO(otherUser.getUsername(),
+                    new ConversationInfoDTO(lastMessageTime, isLastMessageAlreadyRead, numberOfMessages));
+            conversations.add(conversationDTO);
         }
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok().body(conversations);
     }
 }
