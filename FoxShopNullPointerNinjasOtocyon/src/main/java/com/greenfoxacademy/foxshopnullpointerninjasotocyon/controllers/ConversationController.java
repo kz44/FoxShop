6991773c319c5107777db0106e9b554f79f72963
Anna Karner <zanna.karner@gmail.com>
2023-12-02 07:50:16 +0100

    created protected endpoint getAllConversationsOfLoggedInUser()

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/ConversationController.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/ConversationController.java
@@ -1,10 +1,26 @@
 package com.greenfoxacademy.foxshopnullpointerninjasotocyon.controllers;
 
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.models.User;
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.services.MessageService;
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/api/conversations")
+@AllArgsConstructor
 public class ConversationController {
 
+    private UserService userService;
+    private MessageService messageService;
+
+    @GetMapping
+    protected ResponseEntity<?> getAllConversationsOfLoggedInUser() {
+        User user = userService.getUserFromSecurityContextHolder();
+        return ResponseEntity.ok(messageService.getConversationInfo(user));
+    }
 }
