2c6128de795cf03c3660f27cb53de6f5b75f0053
Anna Karner <zanna.karner@gmail.com>
2023-12-02 12:13:11 +0100

    corrected method param

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/services/MessageService.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/services/MessageService.java
@@ -1,8 +1,7 @@
 package com.greenfoxacademy.foxshopnullpointerninjasotocyon.services;
 
-import com.greenfoxacademy.foxshopnullpointerninjasotocyon.models.User;
 import org.springframework.http.ResponseEntity;
 
 public interface MessageService {
-    ResponseEntity<?> getConversationInfo(User user);
+    ResponseEntity<?> getConversationInfo();
 }
