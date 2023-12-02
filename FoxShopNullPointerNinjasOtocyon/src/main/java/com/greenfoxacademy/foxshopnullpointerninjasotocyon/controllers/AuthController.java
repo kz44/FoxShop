e517c01df961c18ee8a7e132dd781072c6840097
Lenka Havlíčková <100620477+LenkaHavlickova@users.noreply.github.com>
2023-12-02 14:44:46 +0100

    Added endpoint for verification.

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/AuthController.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/AuthController.java
@@ -8,10 +8,7 @@ import com.greenfoxacademy.foxshopnullpointerninjasotocyon.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -75,5 +72,10 @@ public class AuthController {
     public ResponseEntity<?> logout() {
         userService.handleSecurityContextAndBlacklistToken();
         return ResponseEntity.ok("Logout successful");
+    }
+
+    @PostMapping("/verify/{userId}/{token}")
+    public ResponseEntity<?> verifyUserEmail(@PathVariable Long userId, @PathVariable String token) {
+        return null;
     }
 }
