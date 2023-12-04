d652a3ea124ff55c395c22bda3eddb3b8e172999
Kollonay Zoltán <zoltan@kollonay.hu>
2023-12-04 14:31:25 +0100

    Feat: -changed @RequestParam to @PathVariable receiverUsername -modify controller endpoints

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/MessageController.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/MessageController.java
@@ -4,10 +4,7 @@ import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.ErrorMessageDTO;
 import com.greenfoxacademy.foxshopnullpointerninjasotocyon.services.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,20 +17,24 @@ public class MessageController {
      * Endpoint for sending a message to a specified user.
      *
      * @param receiverUsername The username of the recipient.
-     * @param content The content of the message.
+     * @param content          The content of the message.
      * @return ResponseEntity containing information about the status of the message sending:
-     *   - 200 OK and details of the sent message in the response body for a successful request with the given username.
-     *   - 400 Bad Request and an error message in the response body for a failed request.
+     * - 200 OK and details of the sent message in the response body for a successful request with the given username.
+     * - 400 Bad Request and an error message in the response body for a failed request.
      */
-    @PostMapping("/send")
-    public ResponseEntity<?> sendMessage(@RequestParam String receiverUsername,
-                                         @RequestParam String content) {
+    @PostMapping(value = {"/send/{receiverUsername}", "/send/", "/send"})
+    public ResponseEntity<?> sendMessage(@PathVariable(required = false) String receiverUsername,
+                                         @RequestParam(required = false) String content) {
 
         if ((receiverUsername == null || receiverUsername.isEmpty()) && (content == null || content.isEmpty())) {
             return ResponseEntity.badRequest().body(new ErrorMessageDTO("Missing username and content"));
-        } else if (receiverUsername == null || receiverUsername.isEmpty()) {
+        }
+
+        if (receiverUsername == null || receiverUsername.isEmpty()) {
             return ResponseEntity.badRequest().body(new ErrorMessageDTO("Missing username"));
-        } else if (content == null || content.isEmpty()) {
+        }
+
+        if (content == null || content.isEmpty()) {
             return ResponseEntity.badRequest().body(new ErrorMessageDTO("Missing content"));
         }
         return messageService.sendMessage(receiverUsername, content);
