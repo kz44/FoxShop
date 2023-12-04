42bb6ed5fd867bcdf3ec3c4dcb9855aab750fa4b
Lenka Havlíčková <100620477+LenkaHavlickova@users.noreply.github.com>
2023-12-04 23:51:22 +0100

    Implemented usage of sendGrid template. Refactoring. Created constant for template id.

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/services/SendGridService.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/services/SendGridService.java
@@ -1,16 +1,13 @@
 package com.greenfoxacademy.foxshopnullpointerninjasotocyon.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.mail.DynamicTemplateData;
-import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.mail.MailContent;
 import com.greenfoxacademy.foxshopnullpointerninjasotocyon.models.User;
 import com.sendgrid.Method;
 import com.sendgrid.Request;
 import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Personalization;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,6 +16,7 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class SendGridService {
 
+    private static final String TEMPLATE_ID = "d-18fb635d86fe4c48bea95781d72c34e0";
     private final PasswordEncoder passwordEncoder;
 
     public void sendVerificationEmail(User user) {
@@ -26,34 +24,32 @@ public class SendGridService {
         Request request = new Request();
         request.setMethod(Method.POST);
         request.setEndpoint("/mail/send");
-        String name = user.getFirstName() != null ? user.getFirstName() : "our new user";
 
-        Email from = new Email("foxshop@post.cz", "FoxShop");
-        String subject = String.format("Thanks for signing up, %s", name);
-        Email to = new Email(user.getEmail());
+        String name = user.getFirstName() != null ? user.getFirstName() : user.getUsername();
+
+        Mail mail = new Mail();
+        Personalization personalization = new Personalization();
+        personalization.addTo(new Email(user.getEmail()));
+        personalization.addDynamicTemplateData("name", name);
         String token = passwordEncoder.encode(user.getUsername());
         String link = "http://localhost:8080/api/auth/verify"
                 .concat("?userId=")
                 .concat(String.valueOf(user.getId()))
                 .concat("&token=").concat(token);
-        DynamicTemplateData dynamicTemplateData = new DynamicTemplateData(name, link);
-        MailContent mailContent = new MailContent(dynamicTemplateData);
-        ObjectMapper mapper = new ObjectMapper();
+        personalization.addDynamicTemplateData("link", link);
+        mail.addPersonalization(personalization);
+        mail.setFrom(new Email("foxshop@post.cz", "FoxShop"));
+        mail.setSubject(String.format("Thanks for signing up, %s", name));
+        mail.setTemplateId(TEMPLATE_ID);
+
         try {
-            String jsonInString = mapper.writeValueAsString(mailContent);
-            Content content = new Content("text/html", String.format("<p>Thanks for signing up, %s. <a href=%s>Click Me</a> </p>", name, link));
-            Mail mail = new Mail(from, subject, to, content);
-//            mail.setTemplateId("d-18fb635d86fe4c48bea95781d72c34e0");
             request.setBody(mail.build());
             Response response = sg.api(request);
             System.out.println(response.getStatusCode());
             System.out.println(response.getBody());
             System.out.println(response.getHeaders());
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-
     }
 }
