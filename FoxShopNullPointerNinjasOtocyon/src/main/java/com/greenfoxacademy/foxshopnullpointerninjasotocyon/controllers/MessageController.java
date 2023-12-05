92ecb1aa05494654f941773b6553c37fccf9de28
Anna Karner <zanna.karner@gmail.com>
2023-12-05 21:40:12 +0100

    simplified showMessagesWithOtherUser endpoint

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/MessageController.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/controllers/MessageController.java
@@ -1,17 +1,13 @@
 package com.greenfoxacademy.foxshopnullpointerninjasotocyon.controllers;
 
-import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.MessagePageableDTO;
 import com.greenfoxacademy.foxshopnullpointerninjasotocyon.services.MessageService;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -24,7 +20,6 @@ public class MessageController {
     protected ResponseEntity<?> showMessagesWithOtherUser(@PathVariable(required = false) String otherUsername,
                                                        @PathVariable(required = false) int pageNumber,
                                                        Pageable pageable) {
-        ResponseEntity<?> messagePage = messageService.getMessagesPagination(otherUsername, pageNumber, pageable);
-        return ResponseEntity.ok().body(messagePage);
+        return ResponseEntity.ok().body(messageService.getMessagesPagination(otherUsername, pageNumber, pageable));
     }
 }
