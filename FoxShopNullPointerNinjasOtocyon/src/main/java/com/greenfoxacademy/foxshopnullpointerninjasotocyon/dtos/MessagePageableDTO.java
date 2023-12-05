e4052bfd6de4e60bcf326a3e2d81db5883be74e1
Anna Karner <zanna.karner@gmail.com>
2023-12-05 18:57:14 +0100

    created MessagePageableDTO

---
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/dtos/MessagePageableDTO.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos;
+
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.models.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+public class MessagePageableDTO {
+    private String content;
+    private LocalDateTime sent;
+    private Long sender;
+    private Long receiver;
+    private boolean isAlreadyRead;
+}
