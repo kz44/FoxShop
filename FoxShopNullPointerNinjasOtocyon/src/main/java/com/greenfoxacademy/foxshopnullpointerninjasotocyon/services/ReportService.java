5e05c2567d0b0ffc97a5e629cd701ce401fd23f1
Lenka Iacovelli <lenkadan@gmail.com>
2023-12-06 16:10:05 +0100

    part 3: updating method names and variable references

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/services/ReportService.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/services/ReportService.java
@@ -7,10 +7,20 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
+package com.greenfoxacademy.foxshopnullpointerninjasotocyon.services;
+
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.ReportCreationDTO;
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos.ReportSummaryDTO;
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.enums.State;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface ReportService {
     ResponseEntity<?> nullCheckReport(ReportCreationDTO reportCreationDTO);
-
     ResponseEntity<?> createNewReport(ReportCreationDTO reportCreationDTO);
-    List<ReportSummaryDTO> reportsToDTOs ();
+    //    List<ReportSummaryDTO> reportsToDTOs ();
     ResponseEntity<?> reportDetails(Long reportID);
+    List<ReportSummaryDTO> browseReportsByUser();
 }
+
