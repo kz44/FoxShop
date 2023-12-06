c12e26bb8688d10e98632196e3aa5af633b000c1
Lenka Iacovelli <lenkadan@gmail.com>
2023-12-06 16:14:54 +0100

    part 5: updating method names and variable references

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/dtos/ReportSummaryDTO.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/dtos/ReportSummaryDTO.java
@@ -1,15 +1,22 @@
 package com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos;
 
+import com.greenfoxacademy.foxshopnullpointerninjasotocyon.models.Report;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 public class ReportSummaryDTO {
     private String reportTitle;
     private Long reportID;
     private String reportStatus;
     private String advertisementTitle;
+
+    public ReportSummaryDTO(Report report){
+        reportTitle = report.getTitle();
+        reportID = report.getId();
+        reportStatus = report.getReportStatus().getState();
+        advertisementTitle = report.getReceiver().getTitle();
+    }
 }
