be742f92189fd7c6063f2b455dc8760cb268bae7
LenkaHavlickova <havlickova.lenka.hl@gmail.com>
2024-02-15 18:32:15 +0100

    Add NoArgsConstructor to MessageDto.

--- FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/dtos/MessageDTO.java
+++ FoxShopNullPointerNinjasOtocyon/src/main/java/com/greenfoxacademy/foxshopnullpointerninjasotocyon/dtos/MessageDTO.java
@@ -2,10 +2,12 @@ package com.greenfoxacademy.foxshopnullpointerninjasotocyon.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class MessageDTO {
 
